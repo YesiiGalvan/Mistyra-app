94139d5d61ed411279a110ab1d95f1150a31f0ce
Yesica Galvan <148825334+YesiiGalvan@users.noreply.github.com>
2025-04-11 11:18:34 -0300

    Agregado archivo App.js con estructura base
    Pantalla principal de bienvenida

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>¡Bienvenida a Mistyra App!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
